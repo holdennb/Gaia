5fe65dacec6754113ed3ba22910dae0f5d66c978
holdennb <nickbholden@gmail.com>
2015-01-29 00:22:27 -0800

    Gets JSON for recent posts at close locations. Rough, uncommented. Going to bed.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ ig.use({ client_id: '3f4a2693d5cc46a4b0686ae1e8df389a',
 app.use(express.static(path.join(__dirname, "public"))); 
 
 // Handle location request
-app.get("/location/:lat/:lng", getResponse);
+app.get("/instagram/:lat/:lng", getInstaResponse);
 
 // Route for everything else.
 app.get("*", function(req, res){
@@ -27,23 +27,54 @@ app.get("*", function(req, res){
 app.listen(3000);
 console.log("Listening on port 3000");
 
-function getResponse(req, res) {
+function getInstaResponse(req, res) {
+    // Search for media posted in the last 2 days within 5000 meters
+    //  of the given location
+    var lat = parseFloat(req.params.lat);
+    var lng = parseFloat(req.params.lng);
 
-	ig.media_search(parseFloat(req.params.lat), parseFloat(req.params.lng),
-		[Date.now() - 1000 * 60 * 60 * 24 * 2, Date.now(), 5000],
-		function(err, medias, remaining, limit) {
-			if (err) {
-				res.send(err);
-			} else {
-				var out = "";
-				for (var i = 0; i < medias.length; i++) {
-					out += "<img src='" + medias[i].images.standard_resolution.url + "'>";
-				}
-				res.send(out);
-			}
-		});
+    ig.location_search({"lat": lat, "lng": lng}, [3000],
+        function(err, locationsResult, remaining, limit) {
+            if (err) {
+                res.send(err);
+            } else {
+                var jsonOut = [];
+                var reqsRemaining = locationsResult.length;
+                for (var i in locationsResult) {
+                    var location = {};
+                    var id = locationsResult[i].id;
+                    location.id = id;
+                    location.name = locationsResult[i].name;
+                    location.posts = [];
 
+                    (function (thisId, thisLocation) {
+                        ig.location_media_recent(thisId,
+                        function(err, mediaResult, pagination, remaining, limit) {
+                            if (err) {
+                                res.send(err);
+                            } else {
+                                for (var j in mediaResult) {
+                                    var thisPost = {};
+                                    thisPost.link = mediaResult[j].link;
+                                    thisLocation.posts.push(thisPost);
+                                }
+                                // inner done
+                                jsonOut.push(thisLocation);
+                                --reqsRemaining;
+                                if (reqsRemaining <= 0) {
+                                    res.json(jsonOut);
+                                }
+                            }
+                        });
+                    })(id, location);
+                    
+                }
+                // done
+            }
+        });
 }
+
+
 
 
 
