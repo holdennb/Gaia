5659b06c89678748a781ff2344dd34ccf5e0739e
holdennb <nickbholden@gmail.com>
2015-02-09 00:33:26 -0800

    Restructured, now gets FB places, has info windows with thumbnails.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-
+var http = require('http');
 var path = require("path");
 var express = require("express");
 var logger = require("morgan");
 var app = express();
 var ig = require("instagram-node").instagram();
+var graph = require('fbgraph');
+
+graph.setAccessToken('1584238351793353|d-YwDPT2Tueuy9zXp1YiCZSSB9k');
+
+// var searchOptions = {
+//     q:     "coffee",
+//     type:  "place",
+//     center: "47.6668621,-122.3163902",
+//     distance: "3000"
+// };
+ 
+// graph.search(searchOptions, function(err, res) {
+//     for (var j in res['data']) {
+//         console.log(res['data'][j].location); // {data: [{id: xxx, from: ...}, {id: xxx, from: ...}]} 
+//     }
+//     // console.log(res['data'].length);
+// });
+
+
+
 
 // Log the requests
 app.use(logger("dev"));
@@ -16,76 +36,103 @@ ig.use({ client_id: '3f4a2693d5cc46a4b0686ae1e8df389a',
 app.use(express.static(path.join(__dirname, "public"))); 
 
 // Handle location request
-app.get("/instagram/:lat/:lng", getInstaResponse);
+app.get("/ig_places/:lat/:lng", get_ig_places);
+app.get("/ig_media/:ig_place_id", get_ig_media);
 
 // Route for everything else.
 app.get("*", function(req, res){
-  res.send("404");
+    console.log("-------- 404 OCCURED");
+    res.send("404");
 });
 
 // Start server
 app.listen(3000);
 console.log("Listening on port 3000");
-
-function getInstaResponse(req, res) {
+var json_out = [];
+var res;
+function get_ig_places(req, result) {
     // Search for media posted in the last 2 days within 5000 meters
     //  of the given location
+    res = result;
     var lat = parseFloat(req.params.lat);
     var lng = parseFloat(req.params.lng);
 
-    ig.location_search({"lat": lat, "lng": lng}, [5000],
-        function(err, locationsResult, remaining, limit) {
-            if (err) {
-                res.send(err);
-            } else {
-                var jsonOut = [];
-                var reqsRemaining = locationsResult.length;
-                for (var i in locationsResult) {
-                    var location = {};
-                    var id = locationsResult[i].id;
-                    location.id = id;
-                    location.name = locationsResult[i].name;
-                    location.lat = locationsResult[i].latitude;
-                    location.lng = locationsResult[i].longitude;
-                    location.posts = [];
-                    var options = {
-                        "min_timestamp": Date.now() - 1000 * 60 * 60 * 24 * 7,
-                        "max_timestamp": Date.now()
-                    };
-                    console.log(options.min_timestamp);
-                    (function (thisId, thisLocation) {
-                        ig.location_media_recent(thisId,
-                            options,
-                        function(err, mediaResult, pagination, remaining, limit) {
-                            if (err) {
-                                res.send(err);
-                            } else {
-                                for (var j in mediaResult) {
-                                    var thisPost = {};
-                                    // thisPost.link = mediaResult[j].link;
-                                    thisPost.date = (new Date(mediaResult[j].created_time * 1000)).toString();
-                                    thisLocation.posts.push(thisPost);
-                                }
-                                // inner done
-                                jsonOut.push(thisLocation);
-                                --reqsRemaining;
-                                if (reqsRemaining <= 0) {
-                                    res.json(jsonOut);
-                                }
-                            }
-                        });
-                    })(id, location);
-                    
-                }
-                // done
-            }
-        });
+
+    var searchOptions = {
+        q:     "parks",
+        type:  "place",
+        center: lat + "," + lng,
+        distance: "3000"
+    };
+    
+    graph.search(searchOptions, get_fb_places);
 }
 
+function get_fb_places(err, fb_res) {
+    console.log("get_fb_places called");
+    if(fb_res.paging && fb_res.paging.next) {
+        graph.get(fb_res.paging.next, get_fb_places);
+    }
+
+    var fb_places_remaining = fb_res['data'].length;
+    // console.log(fb_res);
+    for (var j in fb_res['data']) {
+
+        ig.location_search({"facebook_places_id": fb_res['data'][j].id},
+            function(err, locationsResult, remaining, limit) {
+                if (err) {
+                    console.log("-------- ERROR OCCURED: " + JSON.stringify(err));
+                    res.send(err);
+                } else {
+
+                    for (var i in locationsResult) {
+                        var location = {};
+                        location.ig_place_id = locationsResult[i].id;
+                        console.log(location.ig_place_id);
+                        location.name = locationsResult[i].name;
+                        location.lat = locationsResult[i].latitude;
+                        location.lng = locationsResult[i].longitude;
+                        json_out.push(location);
+                        
+                    }
+                    --fb_places_remaining;
+                    if (fb_places_remaining <= 0) {
+                        console.log("-------- ABOUT TO SEND. YES RESULTS");
+                        res.json(json_out);
+                        return;
+                    }
+                    // done
+
+                }
+            });
+        }
+
+    }
 
 
+function get_ig_media(req, result) {
+    var ig_place_id = req.params.ig_place_id;
+    var json_out = [];
 
+    var options = {
+        "min_timestamp": Date.now() - 1000 * 60 * 60 * 24 * 7,
+        "max_timestamp": Date.now()
+    };
+    ig.location_media_recent(ig_place_id, options,
+        function(err, ig_media_res, pagination, remaining, limit) {
+            if (err) {
+                result.send(err);
+            } else {
+                for (var j in ig_media_res) {
+                    var post = ig_media_res[j];
+                    post.date = (new Date(ig_media_res[j].created_time * 1000)).toString();
+                    json_out.push(post);
+                }
+                result.json(json_out);
+            }
+        });
 
+}
 
 
 
--- public/client.js
+++ public/client.js
@@ -10,27 +10,59 @@ $(document).ready(function() {
             $("#current-lng").text(lng);
 
 
-            $.getJSON("http://127.0.0.1:3000/instagram/" + lat + "/" + lng,
+            $.getJSON("http://127.0.0.1:3000/ig_places/" + lat + "/" + lng,
                 function (data) {
                     // $("#http-response").text(JSON.stringify(data));
 
-                    var myLatlng = new google.maps.LatLng(lat, lng);
+                    var current_loc = new google.maps.LatLng(lat, lng);
                     var mapOptions = {
-                        zoom: 16,
-                        center: myLatlng
+                        zoom: 14,
+                        center: current_loc
                     }
                     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+                    var infowindow = new google.maps.InfoWindow();
 
-                    $.each(data, function(i, val) {
+                    $.each(data, function(i, place) {
+                        // if (place.posts.length > 0) {
+                            // var contentString = "";
 
-                        var marker = new google.maps.Marker({
-                            position: new google.maps.LatLng(val.lat, val.lng),
-                            map: map,
-                            title: 'An instagram location!'
+                            // for (var i in place.posts) {
+                            //     // console.log(place.posts[i]);
+                            //     contentString += "<img src='" +
+                            //         place.posts[i].images.thumbnail.url + 
+                            //         "' /><br />";
+                            // }
+
+
+                            var marker = new google.maps.Marker({
+                                position: new google.maps.LatLng(place.lat, place.lng),
+                                map: map,
+                                title: place.name
+                            });
+                            console.log(marker.ig_place_id);
+                            marker.ig_place_id = place.ig_place_id;
+
+                            google.maps.event.addListener(marker, 'click', function() {
+                                if (!marker.info) {
+                                    $.getJSON("http://127.0.0.1:3000/ig_media/" + marker.ig_place_id,
+                                        function (data) {
+                                            marker.info = "<h1>" + marker.title + "</h1>";
+                                            $.each(data, function(i, post) {
+                                                marker.info += "<img src='" +
+                                                    post.images.thumbnail.url + 
+                                                    "' /><br />";
+                                            });
+                                            infowindow.setContent(marker.info);
+                                            infowindow.open(map,marker);
+                                        });
+                                } else {
+                                    infowindow.setContent(marker.info);
+                                    infowindow.open(map,marker);
+                                }
+                            });
                         });
-                    })
+                    });
                 });
-        });
     } else { 
         // Can't get current location :/
         $("#error p").text("Geolocation is not supported by this browser.");
